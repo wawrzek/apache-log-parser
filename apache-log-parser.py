f4a135ccad242fb85b22d9525c3e1b0f2d956d0e
Wawrzek Niewodniczański <jobs@wawrzek.name>
2012-06-07 23:34:27 +0100

    Adds option ti print UNIX time

--- apache-log-parser.py
+++ apache-log-parser.py
@@ -31,6 +31,8 @@ parser.add_option("-m", "--measure", dest="measure", action="store", type="strin
 but it also cand be easily define for uri, ip address, referral or agent string.
     Prefine responses (status codes) are in range: 200, 201, 202, 203, 204, 205, 206, 300, 301, 302, 303, 304, 305, 306, 307, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 500, 501, 502, 503, 504, 505 \n\
     Prefine request are: GET, POST, OPTIONS, PATCH, PUT, HEAD, CONNECT, DELETE, TRACE """)
+parser.add_option("-t", "--time", dest="timetype", action="store", type="string",
+                help="define how to print time in output - apache - DD/MMM/YYYY, unix - unix timestamp")
 
 (options, args) = parser.parse_args()
 
@@ -38,6 +40,8 @@ if options.measure:
     selected_type = options.measure
 if options.resolution:
     t_resolution = options.resolution
+if options.timetype:
+    timetype = options.timetype
 
 if t_resolution == 'sec':
     time_step = timedelta(seconds=1)
@@ -55,21 +59,23 @@ else :
     print "Wrong step"
     sys.exit(4)
 
-time = datetime.now() - time_step
+time_value = datetime.now() - time_step
    
 if t_resolution == 'sec':
-    time_output = '%H:%M:%s'
-    time_string = datetime.strftime(time, '%d/%b/%Y:%H:%M:%S')
-    time_output = datetime.strftime(time, '%H:%M:%S')
+    time_string = datetime.strftime(time_value, '%d/%b/%Y:%H:%M:%S')
+    time_output = datetime.strftime(time_value, '%H:%M:%S')
 elif t_resolution == 'min':
-    time_string = datetime.strftime(time, '%d/%b/%Y:%H:%M:.{2}')
-    time_output = datetime.strftime(time, '%H:%M')
+    time_string = datetime.strftime(time_value, '%d/%b/%Y:%H:%M:.{2}')
+    time_output = datetime.strftime(time_value, '%H:%M')
 elif t_resolution == 'hour':
-    time_string = datetime.strftime(time, '%d/%b/%Y:%H:.{2}:.{2}')
-    time_output = datetime.strftime(time, '%H')
+    time_string = datetime.strftime(time_value, '%d/%b/%Y:%H:.{2}:.{2}')
+    time_output = datetime.strftime(time_value, '%H')
 elif t_resolution == 'day':
-    time_string = datetime.strftime(time, '%d/%b/%Y:.{2}:.{2}:.{2}')
-    time_output = datetime.strftime(time, '%d/%b/%Y')
+    time_string = datetime.strftime(time_value, '%d/%b/%Y:.{2}:.{2}:.{2}')
+    time_output = datetime.strftime(time_value, '%d/%b/%Y')
+
+if timetype == 'unix':
+    time_output = str(int(time.mktime(time_value.timetuple())))
 
 log_re = '(?P<ip>[.:0-9a-fA-F]+) - - \[%s.{0,6}\] "(?P<request>.*?) (?P<uri>.*?) HTTP/1.\d" (?P<status_code>\d+) \d+ "(?P<referral>.*?)" "(?P<agent>.*?)"'%(time_string)
 
